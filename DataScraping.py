7bac4185927bb5a3ca3802350e3149b8c05caa4d
StefanLam99 <57056657+StefanLam99@users.noreply.github.com>
2021-01-09 15:57:34 +0100

    - Removed small methods: load_data(), write_data() - changed the input and output. - now both get_all_ratings and get_all_ratings_mp work. - added a range to the above mentioned methods, so we can run the methods on different ranges with multiple laptops for example.

--- DataScraping.py
+++ DataScraping.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from requests_html import HTMLSession
+from DataSets import UFS_Universe_NL, zipcode_data_2017, zipcode_data_2019, Neighborhood_Descriptives
 import re
 import multiprocessing as mp
 from datetime import datetime, date
 import time
+
+
+# Note: Make sure to install: pip install -U "urllib3<1.25", otherwise you might get an error.
 
 class RatingScraper:
     data = pd.DataFrame()
@@ -12,17 +16,10 @@ class RatingScraper:
 
     file_output = str
 
-    def __init__(self, file_input, file_output):
-        self.data = self.load_data(file_input)
-        self.file_output = file_output
+    def __init__(self, file_output = 'data_ratings'):
+        self.data = UFS_Universe_NL()
+        self.file_output = 'Data/ScrapedRatings/' + file_output + '.csv'
         self.start = time.time()
-
-    def load_data(self, file_name):
-        data = pd.read_excel(file_name, header =0)
-        return data
-
-    def write_data(self):
-        self.data_ratings.to_csv(self.file_output)
 
     def make_url(self):
         """"
@@ -46,7 +43,7 @@ class RatingScraper:
         For the given url string we start a HTML session. The page is rendered because there are
         Javascript elements on the page. Then we obtain the elements of interest
         """
-        url = url[0]
+        #url = url[0]
         session = HTMLSession()
         r = session.get(url)
         r.html.render()
@@ -58,7 +55,7 @@ class RatingScraper:
         return [rating, no_reviews]
 
 
-    def get_all_ratings(self):
+    def get_all_ratings(self, begin, end):
         """"
         Go through the urls in data.url_list, for every url we call get_rating_for_url and get the
         ratings for that url. It creates the instance variable google_output_df, an dataframe with the ratings
@@ -66,38 +63,42 @@ class RatingScraper:
         """
         google_output = []
         # for url in data.loc[:, 'url_list']:
-        for url in self.data.url_list:
+        for url in self.data.url_list[begin:end]:
             print(url)
+            print(type(url))
             (rating, no_reviews) = self.get_rating_for_url(url)
             google_output.append([rating, no_reviews])
             print(rating, no_reviews)
         google_output = pd.DataFrame(google_output, columns=['rating', 'no_reviews'])
         self.google_output_df = google_output
 
-    def get_all_ratings_mp(self):
+    def get_all_ratings_mp(self, begin, end):
         """"
         Multiprocessing code. Maximum number of processors is used to get all the url data
         I did not implement test to check if it returns the review in the same order as
         with a simple for loop (#TODO?)
         """
         pool = mp.Pool(mp.cpu_count())
-        google_output = pool.map(RatingScraper.get_rating_for_url,[(url,) for url in self.data.url_list])
+        google_output = pool.map(RatingScraper.get_rating_for_url,[url for url in self.data.url_list[begin: end]])
         self.google_output_df = pd.DataFrame(google_output, columns=['rating', 'no_reviews'])
 
-    def scrape(self):
+    def scrape(self, begin = 0, end = 29055, multiprocessing = True): # 29055 is the number of restaurants we have
         """"
         This method is the 'main method' of the class. It calls all the class methods to obtain the final dataframe
         this dataframe then is written to a csv file.
         """
         self.make_url()
-        self.get_all_ratings_mp()
+        if(multiprocessing):
+            self.get_all_ratings_mp(begin, end)
+        else:
+            self.get_all_ratings(begin, end)
+
         self.data_ratings = pd.concat([self.data, self.google_output_df], axis = 1)
-        self.write_data()
+        self.data_ratings.to_csv(self.file_output) # create csv file with the ratings and number of reviews
         print("Succesfully written data. The program run for ", round(time.time() - self.start, 3), " seconds")
 
 
 if __name__ == '__main__':
-    file_input = r"C:\Users\bartd\Erasmus\Erasmus_\Jaar 4\Master Econometrie\Seminar\Code\Data\UFS_Universe_NL.xlsx"
-    file_output = r"C:\Users\bartd\Erasmus\Erasmus_\Jaar 4\Master Econometrie\Seminar\Code\Data\data_ratings.csv"
-    scraper = RatingScraper(file_input, file_output)
-    scraper.scrape()
+    file_output = 'data_ratings' # give ur output csv. file a name
+    scraper = RatingScraper(file_output)
+    scraper.scrape(begin=0, end=5, multiprocessing=True)
